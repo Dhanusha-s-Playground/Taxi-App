4ffea8f083e3cc68edaa4f666a290af4bd4897a4
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-06-12 20:54:46 +0530

    Config category routes basic functionality.

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -5,15 +5,33 @@
 
 const Router = require('@koa/router');
 const router = new Router({prefix: '/api/v1/categories'});
+const CategoryAPI = require('../api/category.api');
 
 /* get all categories. */
-router.get('/', ctx => {
-    ctx.response.body = 'get all works!';
+router.get('/', async ctx => {
+    try {
+        ctx.response.body = await CategoryAPI.getAllCategories();
+    } catch (error) {
+        ctx.response.status = 500;
+    }
 });
 
 /* get category by ID. */
-router.get('/:id', ctx => {
-    ctx.response.body = 'get by id works!';
+router.get('/:id', async ctx => {
+    const id = ctx.request.params.id;
+    try {
+        const result = await CategoryAPI.getCategoryByID(id);
+        if (result) {
+            ctx.response.status = 200;
+            ctx.response.body = result;
+            console.log(result);
+        } else {
+            /* no matching user found. */
+            ctx.response.status = 404;
+        }
+    } catch (error) {
+        ctx.response.status = 500;
+    }
 });
 
 /* update category. */
