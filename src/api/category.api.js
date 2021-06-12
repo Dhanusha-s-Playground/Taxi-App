bef44cef51498fe3c8586d74875e829d12df75c0
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-06-12 20:29:16 +0530

    Add basic methods for the category API.

---
+++ src/api/category.api.js
@@ -0,0 +1,36 @@
+/*
+@author : Dhanusha Perera
+@date : 12/06/2021
+*/
+
+/* get all categories. */
+const getCategory = () => {
+    /* TODO: implementation */
+};
+
+/* get category by ID. */
+const getCategoryByID = () => {
+    /* TODO: implementation */
+};
+
+/* create a new category. */
+const addCategory = () => {
+    /* TODO: implementation */
+};
+
+/* update a existing category. */
+// const updateCategory = () => {
+//
+// };
+
+/* delete a category by ID. */
+// const deleteCategoryByID = () => {
+//
+// };
+
+/* exposing methods. */
+module.exports = {
+    getCategory,
+    getCategoryByID,
+    addCategory
+};
